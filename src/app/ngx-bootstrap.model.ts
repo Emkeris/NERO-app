4027445e88c59350e7898623545c69c311ec3863
emkeris <emkeris@gmail.com>
2019-03-16 12:28:21 +0000

    navigation(simple non-responsive) added

---
+++ src/app/ngx-bootstrap.model.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+@NgModule({
+  imports: [CarouselModule.forRoot()],
+  exports: [CarouselModule],
+})
+export class NgxBootstrapModel { }
